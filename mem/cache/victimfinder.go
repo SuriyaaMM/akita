7bf100aeb8fd7ae3d229bc086948e206182e20a4
SuriyaaMM <ee23b054@iittp.ac.in>
2024-11-09 12:25:49 +0530

    modified victimfinder.go to include MRU eviction

--- mem/cache/victimfinder.go
+++ mem/cache/victimfinder.go
@@ -16,6 +16,7 @@ func NewLRUVictimFinder() *LRUVictimFinder {
 }
 
 // FindVictim returns the least recently used block in a set
+
 func (e *LRUVictimFinder) FindVictim(set *Set) *Block {
 	// First try evicting an empty block
 	for _, block := range set.LRUQueue {
@@ -24,11 +25,27 @@ func (e *LRUVictimFinder) FindVictim(set *Set) *Block {
 		}
 	}
 
+	/*
+		// MRU Eviction
+		// Eviction starts from the back of the queue
+
+		for i := len(set.LRUQueue) - 1; i >= 0; i-- {
+			block := set.LRUQueue[i]
+			if !block.IsLocked {
+				return block
+			}
+		}
+
+		return nil
+	*/
+
+	// LRU Eviction
+
 	for _, block := range set.LRUQueue {
 		if !block.IsLocked {
 			return block
 		}
 	}
 
-	return set.LRUQueue[0]
+	return set.LRUQueue[len(set.LRUQueue)-1]
 }
