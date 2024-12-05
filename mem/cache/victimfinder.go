b724f6534e3fca396709c612f7ff89713e6ef0d5
SuriyaaMM <ee23b054@iittp.ac.in>
2024-12-05 16:59:02 +0530

    modified to original akita

--- mem/cache/victimfinder.go
+++ mem/cache/victimfinder.go
@@ -16,7 +16,6 @@ func NewLRUVictimFinder() *LRUVictimFinder {
 }
 
 // FindVictim returns the least recently used block in a set
-
 func (e *LRUVictimFinder) FindVictim(set *Set) *Block {
 	// First try evicting an empty block
 	for _, block := range set.LRUQueue {
@@ -25,27 +24,11 @@ func (e *LRUVictimFinder) FindVictim(set *Set) *Block {
 		}
 	}
 
-	/*
-		// MRU Eviction
-		// Eviction starts from the back of the queue
-
-		for i := len(set.LRUQueue) - 1; i >= 0; i-- {
-			block := set.LRUQueue[i]
-			if !block.IsLocked {
-				return block
-			}
-		}
-
-		return nil
-	*/
-
-	// LRU Eviction
-
 	for _, block := range set.LRUQueue {
 		if !block.IsLocked {
 			return block
 		}
 	}
 
-	return set.LRUQueue[len(set.LRUQueue)-1]
+	return set.LRUQueue[0]
 }
